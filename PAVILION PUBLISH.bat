f0ed29eb9bf858bdfe3777109fb4fdc0573957e0
Samuel <haveachillday@gmail.com>
2016-10-01 14:21:29 +1000

    no message

--- PAVILION PUBLISH.bat
+++
@@ -1,7 +0,0 @@
-cd %~dp0
-copy ".\MessageEventHandler.cs" "C:\Users\power\AppData\Roaming\SpaceEngineers\Mods\Text To Speech mod\Data\Scripts\folder required here"
-copy ".\SentenceProcession.cs" "C:\Users\power\AppData\Roaming\SpaceEngineers\Mods\Text To Speech mod\Data\Scripts\folder required here"
-copy ".\Pronunciation.cs" "C:\Users\power\AppData\Roaming\SpaceEngineers\Mods\Text To Speech mod\Data\Scripts\folder required here"
-copy ".\SoundPlayer.cs" "C:\Users\power\AppData\Roaming\SpaceEngineers\Mods\Text To Speech mod\Data\Scripts\folder required here"
-copy ".\PrettyScaryDictionary.cs" "C:\Users\power\AppData\Roaming\SpaceEngineers\Mods\Text To Speech mod\Data\Scripts\folder required here"
-copy ".\WordCounter.cs" "C:\Users\power\AppData\Roaming\SpaceEngineers\Mods\Text To Speech mod\Data\Scripts\folder required here"
